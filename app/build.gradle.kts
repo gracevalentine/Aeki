419e9c5eaae3885ab1296efb5cb2e1fe902a27e7
gracevalentine <21gra.ce04@gmail.com>
2025-05-16 21:40:28 +0700

    update frag sign up dan sign in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,15 +42,6 @@ android {
 
 dependencies {
 
-// Retrofit
-    implementation (libs.retrofit2.retrofit)
-
-// Gson converter (untuk parsing JSON otomatis)
-    implementation (libs.retrofit2.converter.gson)
-
-// OkHttp (optional, tapi penting untuk konfigurasi client)
-    implementation (libs.okhttp)
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
