42cd48472416e825e78582dfbe8355cb7c8abbb8
mochamadhabil <abilsastrawinata@gmail.com>
2025-06-08 21:49:26 +0700

    FInal

--- back_end/route/topup_wallet_route.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../Controller/userController');
-
-router.post('/topup', userController.topUpWallet);
--- back_end/route/transactionRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const transactionController = require('../controller/transactionController');
-
-router.post('/cart', transactionController.insertToCart);
-router.get('/:id', transactionController.getTransactionById); // ← ini tambahan route baru
-
-module.exports = router;
